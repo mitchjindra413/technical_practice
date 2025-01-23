6f9f24bca3a7d2078b9059a45ed7244985bd0cc5
Mitchell Jindra <65314998+mitchjindra413@users.noreply.github.com>
2025-01-22 16:03:11 -0800

    Create file

---
+++ 417-PacificAtlanticWaterFlow/417-PacificAtlanticWaterFlow.java
@@ -0,0 +1,55 @@
+class Solution {
+    public record Cords(int r, int c) {
+        public ArrayList<Integer> toArrayList() {
+            ArrayList<Integer> res = new ArrayList<>(List.of(r,c));
+            return res;
+        }
+    }
+
+    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+        
+        int rows = heights.length;
+        int cols = heights[0].length;
+
+        Set<Cords> pac = new HashSet<>();
+        Set<Cords> atl = new HashSet<>();
+
+        for(int c = 0; c < cols; c++) {
+            dfs(heights, 0, c, pac, 0);
+            dfs(heights, rows - 1, c, atl, 0);
+        }
+        for(int r = 0; r < rows; r++) {
+            dfs(heights, r, 0, pac, 0);
+            dfs(heights, r, cols - 1, atl, 0);
+        }
+        System.out.println(pac);
+        List<List<Integer>> res = new ArrayList<>();
+        for(Cords cord : pac) {
+            if(atl.contains(cord)) {
+                res.add(cord.toArrayList());
+            }
+        }
+
+        return res;
+    }
+
+    public void dfs(int[][] heights, int r, int c, Set<Cords> valid, int prev) {
+        Cords key = new Cords(r,c);
+        if(valid.contains(key)) {
+            return;
+        }
+        if(r < 0 || r >= heights.length || c < 0 || c >= heights[0].length) {
+            return;
+        }
+        if(heights[r][c] < prev) {
+            return;
+        }
+
+        valid.add(key);
+        
+        dfs(heights, r + 1, c, valid, heights[r][c]);
+        dfs(heights, r - 1, c, valid, heights[r][c]);
+        dfs(heights, r, c + 1, valid, heights[r][c]);
+        dfs(heights, r, c - 1, valid, heights[r][c]);
+    }
+}
